4317caee1b98a3bb920a2a1f60ddf8ad89203664
adover134 <45279353+adover134@users.noreply.github.com>
2022-05-15 00:27:24 +0900

    Manager에 put 검색 추가 예제

--- tester/views.py
+++ tester/views.py
@@ -27,13 +27,18 @@ class ManagerViewSets(ModelViewSet):
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(self, request, args, kwargs)
 
+    def search(self, request, *args, **kwargs):
+        data = Manager.objects.get(m_tel=request.data['m_tel'])
+        serializer = self.get_serializer(data)
+        return Response(serializer.data)
+
     def partial_update(self, request, *args, **kwargs):
         return super().partial_update(self, request, args, kwargs)
 
     def update(self, request, *args, **kwargs):
         return super().update(self, request, args, kwargs)
 
-    def destroy(self, request, *args, **kwargs):
+    def tere(self, request, *args, **kwargs):
         return super().destroy(self, request, args, kwargs)
 
 
@@ -41,7 +46,7 @@ class UserRetrieveViewSets(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def retrieve(self, request, *args, **kwargs):
+    def search(self, request, *args, **kwargs):
         data = User.objects.get(u_nickname=request.data['u_nickname'])
         serializer = self.get_serializer(data)
         return Response(serializer.data)
