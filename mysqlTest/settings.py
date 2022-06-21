0f89e63e68227f5e5049f1b5b1297201bc1253a7
adover134 <45279353+adover134@users.noreply.github.com>
2022-06-21 11:57:01 +0900

    base 템플릿 추가

--- mysqlTest/settings.py
+++ mysqlTest/settings.py
@@ -125,9 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
