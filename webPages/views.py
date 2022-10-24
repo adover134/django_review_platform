14bc756705f32256caef2baff0046b8432a43107
adover134 <45279353+adover134@users.noreply.github.com>
2022-10-24 14:07:33 +0900

    Update views.py

--- webPages/views.py
+++ webPages/views.py
@@ -317,5 +317,48 @@ def introduction(request):
 def testing(request):
     return render(request, 'room_test.html')
 
+
+@api_view(['POST'])
+def review_write(request):
+    user = request.user
+    review_id = None
+    if request.method == 'POST':
+        data = dict(request.POST)
+        data1 = {}
+        form = customForms.TextReviewWriteForm(request.POST, request.FILES)
+        data1['reviewSentence'] = data['review_sentence']
+        images = request.FILES.getlist('images')
+        print(form)
+        if form.is_valid():
+            print(request.POST)
+            print(request.FILES)
+            data1['reviewTitle'] = data['title']
+            # 주소로 원룸을 검색한다.
+            room = json.loads(requests.get('http://127.0.0.1:8000/db/room/?address=' + data['address'][0]).text)
+            # 만약 없다면, 임의로 주소만 있는 원룸 객체를 만들어서 저장한다.
+            '''
+
+            '''
+            # 원룸 번호를 구한다.
+            print(room)
+            data1['roomId'] = room[0].get('id')
+            data1['uId'] = user.id
+            review = requests.post('http://127.0.0.1:8000/db/review/', data=data1)
+            review_id = json.loads(review.text).get('id')
+            for image in images:
+                img = json.loads(
+                    requests.post('http://127.0.0.1:8000/db/reviewImage/', data={'reviewId': review_id}).text)
+                img_name = handle_uploaded_file(image, str(img.get('id')))
+                requests.put('http://127.0.0.1:8000/db/reviewImage/' + str(img.get('id')) + '/',
+                             data={'reviewId': review_id, 'image': img_name})
+        return Response(str(review_id))
+
+
+def handle_uploaded_file(f, name):
+    with open('static/images/' + name + '.png', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return name + '.png'
+
 def room_write(request):
     return render(request, 'room_write.html')
