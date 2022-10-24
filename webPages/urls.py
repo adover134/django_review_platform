eae80bf4985618b833f5fc9266d58ef47d59ec73
adover134 <45279353+adover134@users.noreply.github.com>
2022-10-24 13:58:23 +0900

    Update urls.py

--- webPages/urls.py
+++ webPages/urls.py
@@ -35,10 +35,14 @@ urlpatterns = [
     path('room_write', views.room_write, name='roomWrite'),
     path('change_user_info/', views.change_user_info),
     path('normal_user_review_list/', views.check_user_reviews, name='wroteReviews'),
-    path('normal_user_room_read/', views.room_read, name='roomTest'),
+    path('normal_user_room_read/', views.room_read, name='roomRead'),
     path('normal_user_room_search/', views.room_search, name='normalUserRoomSearch'),
     path('db/', include('DBs.urls')),
     path('test/', include('webTests.urls')),
     path('review_search/', views.review_search, name='reviewTest'),
-    path('room_test3-1/', views.testing, name='TESTING')
+    path('room_test3-1/', views.testing, name='TESTING'),
+    path('normal_user_review_write/', views.review_write, name='review_write'),
+    path('review_search/', views.review_search, name='reviewTest'),
+    path('introduction/', views.introduction, name='introduction'),
+    path('user_inactivated/', views.user_inactivated),
 ]
