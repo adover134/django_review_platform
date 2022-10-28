07523d39e60fff13708431dc71e9710babe6e6c5
adover134 <45279353+adover134@users.noreply.github.com>
2022-10-28 15:55:22 +0900

    리뷰 열람 페이지 수정

--- static/js/normalUserReviewReadTemplate.js
+++ static/js/normalUserReviewReadTemplate.js
@@ -1,44 +1,44 @@
+// 선택된 아이콘 객체를 넣는다.
 var selected_icon = null;
-var unselect_icon;
+// 아이콘 번호를 넣는다.
+var selected_num;
+// 선택된 아이콘이 있을 경우 해당 아이콘의 미선택 상태의 이미지 위치를 넣는다.
+var unselect_image;
 var sentence = document.getElementById("reviewContent").children;
 var length = sentence.length;
 
 function selectIcon(e){
     if (selected_icon != null) {
-        selected_icon.src = unselect_icon;
+        selected_icon.src = unselect_image;
     }
+    tak()
     if (selected_icon === e.children[0])
-    {
         selected_icon = null;
-        tok();
-    }
     else
     {
         selected_icon = e.children[0];
-        unselect_icon = e.children[2].value;
-        selected_icon.src=e.children[3].value;
-        tok(e.children[1].value);
+        unselect_image = e.children[1].value;
+        selected_icon.src=e.children[2].value;
+        selected_num = e.children[3].value;
+        tok(parseInt(selected_num));
     }
-    console.log(selected_icon);
 }
 
 function tok(a){
-    for (var i=0;i<(length-1);i=(i+1))
-    {
-        if (sentence[i].innerText === a){
-            sentence[i].style.color='blue';
-        }
-        else{
-            sentence[i].style.color='black';
-        }
-    }
+    for(var i=0;i< icons[a].length;i++)
+        document.getElementById('reviewContent').children[icons[a][i]].style.color='#A23355';
+}
+function tak(){
+    var reviews = document.getElementById('reviewContent').children;
+    for(var i=0;i<reviews.length;i++)
+        reviews[i].style.color = 'black';
 }
 const csrftoken = Cookies.get('csrftoken');
 function toggleRecommend(){
     if (reported){
         alert('이미 신고한 글입니다.');
     }
-    else if(userloggedin == false){
+    else if(userloggedin === false){
         alert('로그인이 필요합니다.');
     }
     else {
@@ -110,5 +110,3 @@ function toggleReport(){
         })
     }
 }
-
-console.log(userloggedin);
