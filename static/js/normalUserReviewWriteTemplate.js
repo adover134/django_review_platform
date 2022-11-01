d652fdb8809d38aa4c0055cc0702cc3234a2385c
adover134 <45279353+adover134@users.noreply.github.com>
2022-11-01 15:40:57 +0900

    임시 저장

--- static/js/normalUserReviewWriteTemplate.js
+++ static/js/normalUserReviewWriteTemplate.js
@@ -10,30 +10,52 @@ function to_text(){
 }
 
 $("#text_review").submit(review_submit)
-
+const csrftoken = Cookies.get('csrftoken');
 function review_submit(e) {
     // preventing from page reload and default actions
     e.preventDefault();
     document.getElementById('review_sentence1').value = document.getElementById('review_sentence').innerText
     // serialize the data for sending the form data.
     var form = new FormData(e.currentTarget);
+    const URLSearch = new URLSearchParams(location.search);
+    if (window.location.pathname==='/normal_user_review_change/') {
+        alert("if문-리뷰수정")
+        // var reviewForm = new FormData($('text_review')[0])
+        $.ajax({
+            type: 'POST',
+            url: "/normal_user_review_update/?id="+URLSearch.get('id'),
+            async: false,
+            data: form,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                // on successfull creating object
+                window.location.replace('/normal_user_review_read/?id='+response);
+            },
+            error: function (response) {
+                // alert the error if any error occured
+                alert(response["responseJSON"]["error"]);
+            }
+        })}
+    else{
+        alert("else문-리뷰등록")
     // make POST ajax call
-    $.ajax({
-        type: 'POST',
-        url: "/normal_user_review_write/",
-        async: false,
-        data: form,
-        processData: false,
-        contentType: false,
-        success: function (response) {
-            // on successfull creating object
-            window.location.replace('/normal_user_review_read/?id='+response);
-        },
-        error: function (response) {
-            // alert the error if any error occured
-            alert(response["responseJSON"]["error"]);
-        }
-    })
+        $.ajax({
+            type: 'POST',
+            url: "/normal_user_review_write/",
+            async: false,
+            data: form,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                // on successfull creating object
+                window.location.replace('/normal_user_review_read/?id='+response);
+            },
+            error: function (response) {
+                // alert the error if any error occured
+                alert(response["responseJSON"]["error"]);
+            }
+        })}
 }
 window.onload = function(){
     document.getElementById("id_address").addEventListener("click", function(){ //주소입력칸을 클릭하면
